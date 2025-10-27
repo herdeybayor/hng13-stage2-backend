6417c8b7be5aebdba47b82a0f2eb2e7467cd127b
herdeybayor <shereefdeen4real@gmail.com>
2025-10-27 13:27:07 +0100

    Add migration to make CurrencyCode and ExchangeRate fields optional in Countries table

---
+++ src/CountryCurrencyAPI/Migrations/20251027122423_MakeOptionalCurrencyFields.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CountryCurrencyAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeOptionalCurrencyFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ExchangeRate",
+                table: "Countries",
+                type: "decimal(18,6)",
+                precision: 18,
+                scale: 6,
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,6)",
+                oldPrecision: 18,
+                oldScale: 6);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CurrencyCode",
+                table: "Countries",
+                type: "varchar(10)",
+                maxLength: 10,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(10)",
+                oldMaxLength: 10)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "ExchangeRate",
+                table: "Countries",
+                type: "decimal(18,6)",
+                precision: 18,
+                scale: 6,
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,6)",
+                oldPrecision: 18,
+                oldScale: 6,
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "CurrencyCode",
+                keyValue: null,
+                column: "CurrencyCode",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CurrencyCode",
+                table: "Countries",
+                type: "varchar(10)",
+                maxLength: 10,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(10)",
+                oldMaxLength: 10,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
